18b7951810576aba8b4c1fbfaf447743ff690260
Jo-333 <74973648+Jo-333@users.noreply.github.com>
2021-03-10 08:12:30 +0100

    Add files via upload

--- Esercizi di base2/esercizio2.3.py
+++ Esercizi di base2/esercizio2.3.py
@@ -1,8 +1,24 @@
 #Esercizi di base 2
 #Crea una lista bidimensionale di numeri casuali, tre righe per tre colonne e chiedi all'utente quale riga vuole visualizzare.
+#Scegli riga
 from random import randint
 Riga = 3
 Colonne = 3
+
+lista = []
+for c in range(Riga):
+    Colonna = [randint(1,100), randint(1,100), randint(1,100)]
+    lista.append(Colonna)
+
+print(lista)
+for c in range(len(lista)):
+    print(lista[c])
+selezione_Riga = int(input("Scegli una riga: ")) - 1
+
+
+print(f"L'elemento selezionato è {lista[selezione_Riga]}")
+
+#Scegli elemento
 
 Griglia = []
 for c in range(Riga):
