347005f55ebd0e982948e55b1e0a0a915c37569d
Jo-333 <74973648+Jo-333@users.noreply.github.com>
2021-10-17 18:31:22 +0200

    Update retta.py

--- Coniche/retta.py
+++ Coniche/retta.py
@@ -1,52 +1,70 @@
 class Retta:
     def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+        self.__a = a
+        self.__b = b
+        self.__c = c
 
-    def eqImplicita(self):
+    def getA(self):
+        return self.__a
+
+    def getB(self):
+        return self.__b
+
+    def getC(self):
+        return self.__c
+
+    def eqImplicita(self):  # Applicando i dovuti controlli, ritorna la stringa dell'equazione implicita
         # PRIMO TERMINE
-        incognita_1 = f"{self.a}x" if self.a != 0 else ""
-        incognita_1 = f"x" if self.a == 1 else incognita_1
-        incognita_1 = f"-x" if self.a == -1 else incognita_1
-        incognita_1 = incognita_1 if self.a <= 0 else f"+{incognita_1}"
+        incognita_1 = f"{self.__a}x" if self.__a != 0 else ""
+        incognita_1 = f"x" if self.__a == 1 else incognita_1
+        incognita_1 = f"-x" if self.__a == -1 else incognita_1
+        incognita_1 = incognita_1 if self.__a <= 0 else f"+{incognita_1}"
 
         # SECONDO TERMINE
-        incognita_2 = f"{self.b}y" if self.b != 0 else ""
-        incognita_2 = f"y" if self.b == 1 else incognita_2
-        incognita_2 = f"-y" if self.b == -1 else incognita_2
-        incognita_2 = incognita_2 if self.b <= 0 else f"+{incognita_2}"
+        incognita_2 = f"{self.__b}y" if self.__b != 0 else ""
+        incognita_2 = f"y" if self.__b == 1 else incognita_2
+        incognita_2 = f"-y" if self.__b == -1 else incognita_2
+        incognita_2 = incognita_2 if self.__b <= 0 else f"+{incognita_2}"
 
         # TERMINE NOTO
-        noto = f"{self.c}" if self.c != 0 else ""
-        noto = noto if self.c <= 0 else f"+{noto}"
+        noto = f"{self.__c}" if self.__c != 0 else ""
+        noto = noto if self.__c <= 0 else f"+{noto}"
 
         return f"{incognita_1}{incognita_2}{noto}=0"
 
-    def eqEsplicita(self):
-        if self.b == 0:
-            return "Se b è uguale a 0 non è possibile esplicitare l'equazione."
+    def eqEsplicita(self):  # Applicando i dovuti controlli, ritorna la stringa dell'equazione esplicita
+        if self.__b == 0:
+            raise ZeroDivisionError
 
-        b = self.b if self.b >= 0 else -self.b
+        b = self.__b if self.__b >= 0 else -self.__b
 
         # VARIABILE INDIPENDENTE
-        a = self.a if self.b >= 0 else -self.a
+        a = self.__a if self.__b >= 0 else -self.__a
 
-        ind = f"{a}/{b}x" if abs(self.b) != 1 else f"{a}x"
+        ind = f"{a}/{b}x" if abs(self.__b) != 1 else f"{a}x"
         ind = ind if a <= 0 else f"+{ind}"
         ind = ind if a != 0 else ""
 
         # TERMINE NOTO
-        c = self.c if self.b >= 0 else -self.c
+        c = self.__c if self.__b >= 0 else -self.__c
 
-        noto = f"{c}/{b}" if abs(self.b) != 1 else f"{c}"
+        noto = f"{c}/{b}" if abs(self.__b) != 1 else f"{c}"
         noto = noto if c <= 0 else f"+{noto}"
         noto = noto if c != 0 else ""
 
         return f"y={ind}{noto}"
 
-    def punti(self, x):
-        return x, round(self.a / self.b * x + self.c / self.b, 2)
+    def trovaY(self, x):  # Data un'ascissa, trova la relativa ordinata
+        return x, round(self.__a / self.__b * x + self.__c / self.__b, 2)
+
+    def punti(self, n, m):
+        return [self.trovaY(i) for i in range(min(n, m), max(n, m) + 1)]
+
+    def m(self):  # Ritorna il coefficiente angolare m
+        if self.__b != 0:
+            return self.__a / self.__b
+        else:
+            raise ZeroDivisionError  # Se b = 0, dai un errore
 
 
 def main():
@@ -54,9 +72,7 @@ def main():
                input("Inserisci a, b e c separati da spazi: ").split()]
 
     retta = Retta(a, b, c)
-    print(f"Equazione implicita: {retta.eqImplicita()}")
-    print(f"Equazione esplicita: {retta.eqEsplicita()}")
-    print(f"x e y dato x={retta.punti(float(input('Inserisci x: ')))}")
+    print(type(retta.m()))
 
 
 if __name__ == '__main__':
